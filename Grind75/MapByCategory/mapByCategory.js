5957bb6c205b059cd3c217aa208b8ff46dfd8ffe
jumppack <karan.mehtausa@gmail.com>
2026-03-08 20:56:58 -0400

    Add MapByCategory algorithm solution

---
+++ Grind75/MapByCategory/mapByCategory.js
@@ -0,0 +1,34 @@
+import items from './data.json' with { type: 'json'}
+
+function createMap() {
+
+    const itemMap = new Map();
+
+    items.forEach( (item) => {
+        if (itemMap.has(item.category)) {
+            itemMap.get(item.category).push(item);
+        } else {
+            itemMap.set(item.category, Array.from([item]))
+        }
+    })
+    return itemMap;
+}
+
+const x = createMap();
+console.log(x.keys())
+console.log(count(x.get('fruit')));
+console.log(count(x.get('vegetable')))
+console.log(count(x.get('nut')));
+
+
+console.log(x.get('fruit').filter( (item) => (item.name.lastIndexOf('b'))> -1))
+
+
+
+function count(arr) {
+    // let count = 0;
+    // arr.forEach( _ => count += 1);
+    // return count
+
+    return arr.length;
+}
